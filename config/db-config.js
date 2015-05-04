a45e07a77cd4120882a25ee961d26ddc1e18810d
Tams Sokari <tams.sokari@andela.co>
2015-05-04 06:59:29 +0100

    require local and heroku databases

---
+++ config/db-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      user: 'tams',
+      database: 'jobs'
+    },
+    debug: true,
+  },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      user: 'tams',
+      database: 'jobs-test'
+    },
+    debug: true,
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: 'ec2-54-243-187-196.compute-1.amazonaws.com',
+      user: 'smctxczyocpdxu',
+      database: 'd11sd9hthctjm',
+      port: 5432,
+      password: 'VxWcYWPjM-QhMk8q7XWW17payu'
+    }
+  }
+
+};
